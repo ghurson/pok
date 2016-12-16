e7f16a9380f68b58cb4aec59f2cdb52848173354
Gregg <gregg@topshelfdesign.net>
2016-12-15 21:23:28 -0500

    caught a footer issue. phew!

--- wp-content/themes/pok-master/layout/footer.php
+++ wp-content/themes/pok-master/layout/footer.php
@@ -1,4 +1,3 @@
-</div> <?php // this closes the wrapper div in layout/header.php ?>
 <footer>
     <?php if (get_the_ID() != 1 && get_the_ID() != 13): ?>
         <section class="contact-footer">
@@ -6,20 +5,21 @@
                 <div class="small-12 medium-4 columns">
                     <h5 class="header-no-margin">Chicago Office</h5>
 
-                <p>123 N. Wacker Drive | Suite 1600 | 312.263.9200</p>
-            </div>
+                    <p>123 N. Wacker Drive | Suite 1600 | 312.263.9200</p>
+                </div>
 
-            <div class="small-12 medium-4 columns">
-                <h5 class="header-no-margin">Evanston Office</h5>
+                <div class="small-12 medium-4 columns">
+                    <h5 class="header-no-margin">Evanston Office</h5>
 
-                <p>500 Davis Street | Suite 812 | 847.328.5900</p>
+                    <p>500 Davis Street | Suite 812 | 847.328.5900</p>
+                </div>
+                <div class="small-12 medium-4 columns">
+                    <h5 class="header-no-margin">Email</h5>
+                    <a href="mailto:information@okeefelawoffice.com?">information@okeefelawoffice.com</a>
+                </div>
             </div>
-            <div class="small-12 medium-4 columns">
-                <h5 class="header-no-margin">Email</h5>
-                <a href="mailto:information@okeefelawoffice.com?">information@okeefelawoffice.com</a>
-            </div>
-        </div>
-    </section>
+        </section>
+    <?php endif ?>
     <section class="legal">
         <div class="row">
             <div class="small-12 columns">
